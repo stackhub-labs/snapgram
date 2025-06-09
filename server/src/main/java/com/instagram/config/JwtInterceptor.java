79219d0ee190be51ffb3a1a52d9091711eed2f38
jieunohoh <jeoh@sds.co.kr>
2025-06-09 16:45:05 +0900

    Cors 에러 수정

--- server/src/main/java/com/instagram/config/JwtInterceptor.java
+++ server/src/main/java/com/instagram/config/JwtInterceptor.java
@@ -20,6 +20,11 @@ public class JwtInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) throws Exception {
+        // OPTIONS 요청(preflight)은 JWT 검증을 건너뜁니다
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            return true;
+        }
+        
         String authHeader = request.getHeader("Authorization");
 
         if (authHeader == null || !authHeader.startsWith("Bearer ")) {
