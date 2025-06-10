580d52e101b1240ef8c5d8c8933ea0d33af4e066
신 수정 <jhw02045@naver.com>
2025-06-10 17:51:02 +0900

    게시글 작성 기능 구현 중 [2025-06-10]

--- snapgram-frontend/src/components/board/BoardWrite.jsx
+++ snapgram-frontend/src/components/board/BoardWrite.jsx
@@ -1,18 +1,40 @@
 import Input from "../common/Input";
 import Button from "../common/Button";
 import "../pages/BoardWritePage.css";
+import PlusImage from "../../assets/plus-image.svg";
 import UploadBoard from "../../assets/upload.svg";
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const BoardWrite = () => {
-    const [imgFile, setImgFile] = useState('');
+    const fileInputRef = useRef(null);
+    const [preview, setPreview] = useState(null);
+
+    const handleFileChange = (e) => {
+        const file = e.target.files[0];
+        if (file && file.type.startsWith("image/")) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setPreview(reader.result);
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
     return (
         <div className ="input-tag-outline">
             <div className="input-tag">
                 <Input type="file" 
                     accept='image/jpg, image/png, image/jpeg, image/gif'
-                    name='board_upload_img'/>
+                    name='board_upload_img'
+                    style={{display: "none"}}
+                    ref={fileInputRef}
+                    onChange={handleFileChange}
+                    />
                 <Input placeholder={"게시글 입력란"}/>
+                <Button style={{width: "50px"}}
+                    onClick={() => fileInputRef.current.click()}>
+                        <img src={PlusImage} alt="이미지 선택"/>
+                </Button>
                 <Button style={{width: "50px"}}>
                     <img src={UploadBoard} alt="업로드 아이콘"/>
                 </Button>
@@ -21,3 +43,7 @@ const BoardWrite = () => {
     );
 }
 export default BoardWrite;
+
+// https://moretz0921.tistory.com/770
+// https://velog.io/@min1378/React-image-%EC%97%85%EB%A1%9C%EB%93%9Cs
+// 이미지 인풋 태그에 onChange 이벤트 넣기
